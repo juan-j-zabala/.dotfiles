3f0b37f8948d7600429a88732e0a486985c07ab9
juan-j-zabala <juzadev217@gmail.com>
2024-01-09 16:58:49 -0500

    second theme purple

--- .p10k.zsh
+++ .p10k.zsh
@@ -185,7 +185,7 @@
   #################################[ os_icon: os identifier ]##################################
   # OS identifier color.
   typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=232
-  typeset -g POWERLEVEL9K_OS_ICON_BACKGROUND=044
+  typeset -g POWERLEVEL9K_OS_ICON_BACKGROUND=057
   # Custom icon.
    typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION='󰹁'
 
@@ -216,17 +216,17 @@
   # Current directory background color.
   typeset -g POWERLEVEL9K_DIR_BACKGROUND=232
   # Default current directory foreground color.
-  typeset -g POWERLEVEL9K_DIR_FOREGROUND=044
+  typeset -g POWERLEVEL9K_DIR_FOREGROUND=057
   # If directory is too long, shorten some of its segments to the shortest possible unique
   # prefix. The shortened directory can be tab-completed to the original.
   typeset -g POWERLEVEL9K_SHORTEN_STRATEGY=truncate_to_unique
   # Replace removed segment suffixes with this symbol.
   typeset -g POWERLEVEL9K_SHORTEN_DELIMITER=
   # Color of the shortened directory segments.
-  typeset -g POWERLEVEL9K_DIR_SHORTENED_FOREGROUND=250
+  typeset -g POWERLEVEL9K_DIR_SHORTENED_FOREGROUND=057
   # Color of the anchor directory segments. Anchor segments are never shortened. The first
   # segment is always an anchor.
-  typeset -g POWERLEVEL9K_DIR_ANCHOR_FOREGROUND=044
+  typeset -g POWERLEVEL9K_DIR_ANCHOR_FOREGROUND=057
   # Display anchor directory segments in bold.
   typeset -g POWERLEVEL9K_DIR_ANCHOR_BOLD=true
   # Don't shorten directories that contain any of these files. They are anchors.
--- .zshrc
+++ .zshrc
@@ -118,3 +118,6 @@ source ~/powerlevel10k/powerlevel10k.zsh-theme
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
+# Following line was automatically added by arttime installer
+export PATH=/home/juanzr/.local/bin:$PATH
