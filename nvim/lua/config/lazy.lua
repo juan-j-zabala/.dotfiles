00ff48d0b73f91107629e4cf54c7b9b6e5a3ab97
juan-j-zabala <juzadev217@gmail.com>
2024-02-11 06:50:56 -0500

    firs commit dark-theme

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -9,17 +9,7 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    {
-      "LazyVim/LazyVim",
-      import = "lazyvim.plugins",
-      opts = {
-        colorscheme = "solarized-osaka",
-        news = {
-          lazyvim = true,
-          neovim = true,
-        },
-      },
-    },
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,23 @@
+-- return {
+--   {
+--     "craftzdog/solarized-osaka.nvim",
+--     lazy = true,
+--     priority = 1000,
+--     opts = function()
+--       return {
+--         transparent = true,
+--       }
+--     end,
+--   },
+-- }
+--
 return {
-  {
-    "craftzdog/solarized-osaka.nvim",
-    lazy = true,
-    priority = 1000,
-    opts = function()
-      return {
-        transparent = true,
-      }
-    end,
+  "folke/tokyonight.nvim",
+  opts = {
+    transparent = true,
+    styles = {
+      sidebars = "transparent",
+      floats = "transparent",
+    },
   },
 }
